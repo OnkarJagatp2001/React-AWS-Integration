c6566e36ab4b50700430c234120f09eed3acb248
onkar <onkarbjagtap2001@gmail.com>
2023-11-06 15:42:48 +0530

    "New line is added"

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <h1>Hello I am Onkar Jagtap</h1>
       <h2>Roll No 20141261</h2>
       <h3>This is experiment demo</h3>
+      <h2 style={{ color: "red" }}>
+        This is hosting of react app on AWS amplify
+      </h2>
     </div>
   );
 }
